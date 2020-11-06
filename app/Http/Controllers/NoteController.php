3a4a7f4c04f2494dbfdd0d59b43c4ce12c9de4d0
Mubarak Awal <awalmubarak6@gmail.com>
2020-11-06 15:00:24 +0000

    update validation rules and returned routes

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -8,20 +8,20 @@ use App\Note;
 class NoteController extends Controller
 {
     function getAllNotes(){
-        $notes = Note::latest();
+        $notes = Note::orderBy('created_at', 'desc')->get();
         return view('note.notes', ['notes'=>$notes]);
     }
 
     function addNote(Request $request){
         $request->validate([
-            'title'=>'required',
+            'title'=>'required|max:100',
             'body'=>'required'
         ]);
 
         $noteData = $request->only(['title', 'body']);
         $note = Note::create($noteData);
         if($note){
-            return redirect()->route('notes')->with('success', 'Note created successfully');
+            return redirect()->route('notes.all')->with('success', 'Note created successfully');
         }else{
             return back()->with('error', 'Error occured while adding note. please try again');
         }
@@ -30,14 +30,14 @@ class NoteController extends Controller
 
     function updateNote(Request $request, $id){
         $request->validate([
-            'title'=>'required',
+            'title'=>'required|max:100',
             'body'=>'required'
         ]);
         $note = Note::find($id);
         if($note){
             $noteData = $request->only(['title', 'body']);
             $note->update($noteData);
-            return redirect()->route('notes')->with('success', 'Note updated successfully');
+            return redirect()->route('notes.all')->with('success', 'Note updated successfully');
         }else{
             return back()->with('error', 'Error occured while updating note. please try again');
         }
@@ -49,7 +49,7 @@ class NoteController extends Controller
 
         $note = Note::find($id);
         if($note->delete()){
-            return redirect()->route('notes')->with('success', 'Note deleted successfully');
+            return redirect()->route('notes.all')->with('success', 'Note deleted successfully');
         }else{
             return back()->with('error', 'Error occured while deleting note. please try again');
         }
