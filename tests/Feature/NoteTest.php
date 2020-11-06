26cef6e2f2ac5e7d8b3af21b632732b397342cf5
Mubarak Awal <awalmubarak6@gmail.com>
2020-11-06 16:23:11 +0000

    refactor to use note factor and test note crud

--- tests/Feature/NoteTest.php
+++ tests/Feature/NoteTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Note;
 
 class NoteTest extends TestCase
 {
@@ -16,23 +15,32 @@ class NoteTest extends TestCase
      * @return void
      */
 
-    public $notesArray = [
-        ['title'=>'hello there', 'body'=>'this is a long body'],
-        ['title'=>'hello there2', 'body'=>'this is a long body again'],
-    ];
-
-    public function setUp(): void{
-        parent::setUp();
-        foreach($this->notesArray as $note){
-            Note::create($note);
-        }
-    }
-
 
     public function test_if_can_retrieve_all_notes(){
-        $response = $this->get('/notes');
-        $response->assertStatus(200);
-        $response ->assertSeeTextInOrder([$this->notesArray[0]['title'], $this->notesArray[0]['body'], $this->notesArray[1]['title'], $this->notesArray[1]['body']]);
+        $note = factory('App\Note')->create();
+        $response = $this->get(route('notes.all'));
+        $response->assertStatus(200)
+            ->assertSeeText($note->title)
+            ->assertSeeText($note->body);
+    }
+
+    public function test_if_can_add_new_note(){
+        $newNote = ['title'=>'new note title', 'body'=>'and here is a long body'];
+        $response = $this->post(route('notes.add'), $newNote);
+        $response->assertStatus(302)->assertRedirect(route('notes.all'));
+    }
+
+    public function test_if_can_delete_note(){
+        $note = factory('App\Note')->create();
+        $response = $this->delete(route('notes.delete', $note));
+        $response->assertStatus(302)->assertRedirect(route('notes.all'));
+    }
+
+    public function test_if_can_update_note(){
+        $note = factory('App\Note')->create();
+        $newNote = ['title'=>'new note title', 'body'=>'and here is a long body'];
+        $response = $this->put(route('notes.update', $note), $newNote);
+        $response->assertStatus(302)->assertRedirect(route('notes.all'));
     }
 
 }
