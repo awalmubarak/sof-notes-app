d7e1e5deea1b23461b886849fed75c174faa3766
Mubarak Awal <awalmubarak6@gmail.com>
2020-11-06 15:12:39 +0000

    add json response support

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -9,6 +9,9 @@ class NoteController extends Controller
 {
     function getAllNotes(){
         $notes = Note::orderBy('created_at', 'desc')->get();
+        if ($request->wantsJson()) {
+            return response()->json($notes, 200);
+        }
         return view('note.notes', ['notes'=>$notes]);
     }
 
@@ -21,8 +24,14 @@ class NoteController extends Controller
         $noteData = $request->only(['title', 'body']);
         $note = Note::create($noteData);
         if($note){
+            if($request->wantsJson()) {
+                return response()->json($note, 201);
+            }
             return redirect()->route('notes.all')->with('success', 'Note created successfully');
         }else{
+            if ($request->wantsJson()) {
+                return response()->json(['error'=>'failed to add note'], 400);
+            }
             return back()->with('error', 'Error occured while adding note. please try again');
         }
          
@@ -37,8 +46,14 @@ class NoteController extends Controller
         if($note){
             $noteData = $request->only(['title', 'body']);
             $note->update($noteData);
+            if ($request->wantsJson()) {
+                return response()->json($note, 201);
+            }
             return redirect()->route('notes.all')->with('success', 'Note updated successfully');
         }else{
+            if ($request->wantsJson()) {
+                return response()->json(['error'=>'failed to update note'], 400);
+            }
             return back()->with('error', 'Error occured while updating note. please try again');
         }
         
@@ -49,8 +64,14 @@ class NoteController extends Controller
 
         $note = Note::find($id);
         if($note->delete()){
+            if ($request->wantsJson()) {
+                return response()->json($note, 200);
+            }
             return redirect()->route('notes.all')->with('success', 'Note deleted successfully');
         }else{
+            if ($request->wantsJson()) {
+                return response()->json(['error'=>'failed to delete note'], 400);
+            }
             return back()->with('error', 'Error occured while deleting note. please try again');
         }
         
