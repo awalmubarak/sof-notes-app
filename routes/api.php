4bc63c5162d9fbb40b87350a557d75f63ab2e32a
Mubarak Awal <awalmubarak6@gmail.com>
2020-11-06 15:13:46 +0000

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/notes', 'NoteController@getAllNotes');
+Route::post('/notes', 'NoteController@addNote');
+Route::put('/notes/{id}', 'NoteController@updateNote');
+Route::delete('/notes/{id}', 'NoteController@deleteNote');
+
